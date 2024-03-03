73fc423878558a8d0feeee79e17a6ab349c1ce1d
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2024-03-03 15:56:26 +0100

    Update data

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,19 +1,21 @@
 "use client";
 
-import { ChevronsLeft, MenuIcon, PlusCircle } from "lucide-react";
+import { ChevronsLeft, MenuIcon, PlusCircle, Search, Settings } from "lucide-react";
 import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { UserItem } from "@/app/(main)/_components/user-item";
-import { useQuery } from "convex/react";
+import { useMutation, useQuery } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "@/app/(main)/_components/item";
+import { toast } from "sonner";
 
 export default function Navigation() {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
     const documents = useQuery(api.documents.get, {});
+    const create = useMutation(api.documents.create);
 
     const isResizingRef = useRef(false);
     const sidebarRef = useRef<ElementRef<"aside">>(null);
@@ -96,6 +98,16 @@ export default function Navigation() {
         }
     }
 
+    const handleCreate = () => {
+        const promise = create({ title: "Untitled" });
+
+        toast.promise(promise, {
+            loading: "Creating a new note...",
+            success: "New note created!",
+            error: "Failed to create a new note",
+        });
+    }
+
     return (
         <>
             <aside
@@ -115,7 +127,19 @@ export default function Navigation() {
                 <div>
                     <UserItem/>
                     <Item
+                        label="Search"
+                        icon={Search}
+                        isSearch
                         onClick={() => {}}
+                    />
+                    <Item
+                        label="Settings"
+                        icon={Settings}
+                        isSearch
+                        onClick={() => {}}
+                    />
+                    <Item
+                        onClick={handleCreate}
                         label="New page"
                         icon={PlusCircle}
                     />
